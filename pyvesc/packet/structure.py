c1afa3d2f5eaf1d9f9805a823a85d7a305ade5cf
Yongmin Lee <lym7505@kaist.ac.kr>
2019-09-17 17:50:37 +0900

    edited fmt function with regard to start byte

--- pyvesc/packet/structure.py
+++ pyvesc/packet/structure.py
@@ -40,12 +40,12 @@ class Header(collections.namedtuple('Header', ['payload_index', 'payload_length'
         :param start_byte: The first byte in the buffer.
         :return: The character format of the packet header.
         """
-        if start_byte is 0x2:
+        if start_byte is 0x2 or start_byte == b'\x02':
             return '>BB'
-        elif start_byte is 0x3:
+        elif start_byte is 0x3 or start_byte == b'\x03':
             return '>BH'
         else:
-            raise CorruptPacket("Invalid start byte: %u" % start_byte)
+            raise CorruptPacket("Invalid start byte: {}".format(start_byte))
 
 
 class Footer(collections.namedtuple('Footer', ['crc', 'terminator'])):
